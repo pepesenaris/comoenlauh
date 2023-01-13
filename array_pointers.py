69a305807bd53b2367786a594daf1fd0fd032875
Jose Javier Senaris Carballo <pepe.senaris@alayacare.com>
2023-01-12 20:38:41 -0500

    Play around

---
+++ array_pointers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def target_sum(sorted_array):
+    """
+    Given an array of sorted numbers and a target sum,
+    find a pair in the array whose sum is equal to the given target.
+    """
+    pass
