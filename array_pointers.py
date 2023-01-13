388543b96ead8a6ad71359581178e3632d89c349
Jose Javier Senaris Carballo <pepe.senaris@alayacare.com>
2023-01-12 20:49:53 -0500

    Simple target sum

--- array_pointers.py
+++ array_pointers.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 
-def target_sum(sorted_array):
+
+def find_pair_that_sum(sorted_array, target):
     """
     Given an array of sorted numbers and a target sum,
     find a pair in the array whose sum is equal to the given target.
     """
-    pass
+    left = 0
+    right = len(sorted_array) - 1
+
+    while left <= right:
+        current_sum = sorted_array[left] + sorted_array[right]
+        if current_sum == target:
+            return (sorted_array[left], sorted_array[right])
+        elif current_sum < target:
+            left += 1 # We need to try with a larger number
+        else:
+            right -= 1 # Look for a smaller number to approach the target
+    return (None, None)
+
+
+if __name__ == "__main__":
+    array = [ 1, 32, 64, 122, 877, 1009]
+    print(f"Given {array}")
+    for target in [154, 20]:
+        print(f"Find 2 numbers that sum {target}: {find_pair_that_sum(array, target)}")
