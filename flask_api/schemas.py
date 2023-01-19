e3179e7605220ae66cca626c0409640175f1bf28
Jose Javier Senaris Carballo <pepe.senaris@alayacare.com>
2023-01-18 19:54:40 -0500

    Set marshmallow schema

---
+++ flask_api/schemas.py
@@ -0,0 +1,13 @@
+from config import db, ma
+from models import Person
+
+
+class PersonSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        load_instance = True
+        sqla_session = db.session
+
+
+person_schema = PersonSchema()
+people_schema = PersonSchema(many=True)
