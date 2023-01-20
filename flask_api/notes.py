d777c244e9fb1277b64479f9d78f8c839fcb7c0e
Jose Javier Senaris Carballo <pepe.senaris@alayacare.com>
2023-01-19 20:33:17 -0500

    Create note endpoint

--- flask_api/notes.py
+++ flask_api/notes.py
@@ -1,6 +1,6 @@
 from config import db
 from flask import abort, make_response
-from models import Note
+from models import Note, Person
 from schemas import note_schema
 
 
@@ -11,6 +11,19 @@ def read_all():
 
 def _get_by_id(note_id):
     return Note.query.get(note_id)
+
+
+def create(note):
+    person_id = note.get("person_id")
+    person = Person.query.get(person_id)
+
+    if person:
+        new_note = note_schema.load(note, session=db.session)
+        person.notes.append(new_note)
+        db.session.commit()
+        return note_schema.dump(new_note), 201
+    else:
+        abort(404, f"Person not found for ID: {person_id}")
 
 
 def read_one(note_id):
