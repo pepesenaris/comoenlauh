7f7595e0c03a087a4d603a708f04c2a7d894250a
Jose Javier Senaris Carballo <pepe.senaris@alayacare.com>
2023-01-22 12:23:28 -0500

    WIP slq

--- sql.sql
+++ sql.sql
@@ -32,3 +32,19 @@
 
 -- 3 - For the project “Build a cool site”, if an employee was paid on the 1st and the 15th of every month, show how much each employee made for the duration of the project.
 
+-- Jose Javier Señaris Carballo ran 46 lines of PostgreSQL (finished in 105m
+
+--  first_name | last_name | salary | biweekly_salary 
+-- ------------+-----------+--------+-----------------
+--  John       | Smith     |  20000 |             833
+--  Ava        | Muffinson |  10000 |             416
+--  Cailin     | Ninson    |  30000 |            1250
+--  Mike       | Peterson  |  20000 |             833
+--  Ian        | Peterson  |  80000 |            3333
+--  John       | Mills     |  50000 |            2083
+-- (6 rows)
+
+--        title       | start_date |  end_date  |      age       
+-- -------------------+------------+------------+----------------
+--  Build a cool site | 2011-10-28 | 2012-01-26 | 2 mons 29 days
+-- (1 row)
